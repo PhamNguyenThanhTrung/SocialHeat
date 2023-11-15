6baf75879a54c8d56d6e34d8fa186bf94c06aa68
PhamNguyenThanhTrung <trunghhf@gmail.com>
2023-11-15 15:30:04 +0700

    First commit

---
+++ layout/layout.php
@@ -0,0 +1,16 @@
+<!-- layout.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Các liên kết CSS và các thẻ meta -->
+</head>
+<body>
+    <nav>
+        <!-- Menu của bạn -->
+    </nav>
+    <div class="container">
+        <!-- Hiển thị nội dung từ trang con tại đây -->
+        <?= $this->renderSection('content') ?>
+    </div>
+</body>
+</html>
